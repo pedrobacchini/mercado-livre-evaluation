b2680e07ff60d2bcb302e3b5d99eb4a712f4b19a
Pedro Bacchini <pedrobacchini@outlook.com>
2021-06-26 17:57:14 -0400

    Create project.

---
+++ src/main/kotlin/com/github/pedrobacchini/mercadolivreevaluation/MercadoLivreEvaluationApplication.kt
@@ -0,0 +1,11 @@
+package com.github.pedrobacchini.mercadolivreevaluation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MercadoLivreEvaluationApplication
+
+fun main(args: Array<String>) {
+	runApplication<MercadoLivreEvaluationApplication>(*args)
+}
